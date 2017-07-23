8128e81a14e6d0f9721065edf3ae480a648cc76b
ZaymonFC <generation281@gmail.com>
2017-07-23 22:36:30 +1000

    Understood working with phaserjs sprites and implemented a 'game' GameState

--- js/main.js
+++ js/main.js
@@ -1,22 +1,65 @@
-window.onload = function() {
+window.onload = function () {
 
-        //  Note that this html file is set to pull down Phaser 2.5.0 from the JS Delivr CDN.
-        //  Although it will work fine with this tutorial, it's almost certainly not the most current version.
-        //  Be sure to replace it with an updated version before you start experimenting with adding your own code.
+    // Define a config variable to store configurable properties
+    var gameConf = {
+        height: 800,
+        width: 600,
+        boardHeight: 10,
+        boardWidth: 10,
+        mineCount: 10,
+        tileDimension: 46,
+        edgeOffset: 10
+    }
 
-        var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+    var game = new Phaser.Game(gameConf.height, gameConf.width, Phaser.AUTO, '')
 
-        function preload () {
 
-            game.load.image('logo', 'phaser.png');
 
-        }
+    var gameState = {
 
-        function create () {
+        // Shift assets to memory
+        preload: function () {
+            // Load the Square tileset
+            game.load.image('tile', 'assets/square tile.svg')
 
-            var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-            logo.anchor.setTo(0.5, 0.5);
+            // Load the hexagonal tileset
+
+        },
+
+        // Create elements
+        create: function () {
+            // Set the stage
+            game.stage.backgroundColor = "F19C79"
+            // Create the grid
+            
+            var grid = makeGrid();
+            game.add.group(grid);
+
+            // Populate the mines
+
+            // Start Scoring
+
+            // Handle clicks
             
         }
+    }
 
-    };
+    function makeGrid() {
+        grid = game.add.group()
+        for (var i = 0; i < gameConf.boardHeight; i++) {
+            for (var j = 0; j < gameConf.boardWidth; j++) {
+                grid.create(j * gameConf.tileDimension + gameConf.edgeOffset, i * gameConf.tileDimension + gameConf.edgeOffset, 'tile');
+            }
+        }
+        return grid
+    }
+
+
+    // Run the initial gamestate
+
+    // Main Menu
+
+
+    game.state.add('Game', gameState)
+    game.state.start('Game')
+};
