dae40380f85bea91d8eea6ffadb08a158a434dd2
ZaymonFC <generation281@gmail.com>
2017-07-24 20:10:59 +1000

    Configured node modules and installed webpack

--- js/GameTile.js
+++ js/GameTile.js
@@ -1,5 +1,18 @@
 export default class Tile {
-    constructor (gameRef, x, y, tileSvg, type, i, j) {
+
+
+    constructor(gameRef, x, y, type, i, j) {
+        // Store the different images in a variable
+        this.svgs = {
+            square_up: "assets/square_up.svg",
+            square_down: "assets/",
+            square_bomb: "assets/",
+            square_covered: "assets/",
+            hex_up: "assets/",
+            hex_down: "assets/",
+            hex_bomb: "assets/",
+            hex_covered: "assets/",
+        }
         this.anchor.setTo(0.5, 0.5)
         this.tileText = game.make.text(0, 0, type)
 
