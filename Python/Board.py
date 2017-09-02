c4c8213634f36590513f0d2ca71df7494076d820
ZaymonFC <generation281@gmail.com>
2017-09-02 18:29:57 +1000

    Added encoding for epydoc2

--- Python/Board.py
+++ Python/Board.py
@@ -1,4 +1,4 @@
-# coding=<utf-8>
+# coding=utf-8
 #####   ---------------------------------------------------------------   #####
 ##                               Board Class                                 ##
 ##                Contains Model Representation of a Game Board              ##
--- Python/GameController.py
+++ Python/GameController.py
@@ -1,4 +1,4 @@
-# coding=<utf-8>
+# coding=utf-8
 #####   ---------------------------------------------------------------   #####
 ##                          Controller Class                                 ##
 ##               Entire Game Implementation for Minesweeper                  ##
