f0f2b557eb29de12866e4bcac9a57333696921e8
ZaymonFC <generation281@gmail.com>
2017-09-22 15:00:06 +1000

    Finalized file names and dir structure

--- Python/AppFrame.py
+++
@@ -1,9 +0,0 @@
-#
-# ─── APPLICATION FRAME CLASS ────────────────────────────────────────────────────
-# ─── WILL BE POPULATED WITH DIFFERENT VIEWS AT RUNTIME ──────────────────────────
-#
-
-import tkinter as tk
-
-class AppFrame(tk.Frame):
-    
